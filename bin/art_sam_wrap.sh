5c23db404d01b06e6e145e8d277ab4a47c69bd2e
Marc Mengel <mengel@fnal.gov>
2013-02-08 11:08:33 -0600

    check space

--- bin/art_sam_wrap.sh
+++ bin/art_sam_wrap.sh
@@ -42,6 +42,17 @@ do
     break
 done
 
+check_space() {
+   set : `df -P . | tail -1`
+   avail=$5
+   if [ $avail -lt 100 ] 
+   then
+       echo "Not enough space on this node in `pwd`."
+       df -H .
+       exit 1
+   fi
+}
+
 if [ "x$IFDH_ART_DIR" = "x" ] 
 then
     . `ups setup ifdh_art $vers -q $quals:`
@@ -65,7 +76,10 @@ consumer_id=`ifdh establishProcess "$projurl" "$cmd" "$ART_VERSION" "$hostname" 
 echo project url: $projurl
 echo consumer id: $consumer_id
 
-if getconfig
+cd $TMPDIR
+check_space()
+
+if $getconfig
 then
 
     uri=`ifdh getNextFile $projurl $consumer_id`
@@ -76,6 +90,7 @@ then
         conf="$fname"
 	ifdh updateFileStatus $cpurl  $consumer_id $fname transferred
 	command="\"${cmd}\" -c \"$conf\" $args"
+        echo "Running: $command"
 	if eval "$command"
         then 
 	   ifdh updateFileStatus $cpurl  $consumer_id $fname consumed
@@ -85,35 +100,35 @@ then
         fi
         uri=`ifdh getNextFile $projurl $consumer_id`
     done
-else
-
-update_via_fcl=true
-
-if $update_via_fcl
-then
-
-    cp $conf ${TMPDIR:=/var/tmp}/conf.$$
-    conf=$TMPDIR/conf.$$
-    cat >> $conf <<EOF
-services.user.IFDH: {}
-services.user.IFDH.debug: "1"
-services.user.CatalogInterface.service_provider: "IFCatalogInterface"
-services.user.CatalogInterface.webURI: "$projurl"
-services.user.FileTransfer.service_provider: "IFFileTransfer"
-source.fileNames: [ "$consumer_id" ]
-EOF
 
 else
-    args="$args \"--sam-web-uri=$projurl\" \"--sam-process-id=$consumer-id\""
-fi
+
+    update_via_fcl=true
+
+    if $update_via_fcl
+    then
+
+	cp $conf ${TMPDIR:=/var/tmp}/conf.$$
+	conf=$TMPDIR/conf.$$
+	cat >> $conf <<EOF
+    services.user.IFDH: {}
+    services.user.IFDH.debug: "1"
+    services.user.CatalogInterface.service_provider: "IFCatalogInterface"
+    services.user.CatalogInterface.webURI: "$projurl"
+    services.user.FileTransfer.service_provider: "IFFileTransfer"
+    source.fileNames: [ "$consumer_id" ]
+    EOF
+
+    else
+	args="$args \"--sam-web-uri=$projurl\" \"--sam-process-id=$consumer-id\""
+    fi
 
 
-cd $TMPDIR
 
-command="\"${cmd}\" -c \"$conf\" $args"
-echo "Running: $command"
-eval "$command"
-res=$?
+    command="\"${cmd}\" -c \"$conf\" $args"
+    echo "Running: $command"
+    eval "$command"
+    res=$?
 
 fi
 
