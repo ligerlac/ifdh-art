6e0dc5b98739e87d086f84a42dc184aeeaa2fffb
Marc Mengel <mengel@fnal.gov>
2013-04-12 10:57:40 -0500

    sigh

--- ups/build_node_setup.sh
+++ ups/build_node_setup.sh
@@ -1,9 +1,9 @@
 
 case `hostname` in
-minervagpvm02*) export buildargs="debug e2"
+novagpvm02*) export buildargs="debug e2"
                 export DECLAREBITS="-q debug:e2 -4"
                 ;;
-minervagpvm03*) export buildargs="prof e2"
+novagpvm03*) export buildargs="prof e2"
                 export DECLAREBITS="-q e2:prof -4"
                  ;;
 esac
