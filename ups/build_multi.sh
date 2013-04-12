66b5978634ac8775732e8819302286ab3f61887b
Marc Mengel <mengel@fnal.gov>
2013-04-12 11:10:49 -0500

    test updates to build_multi.sh

--- ups/build_multi.sh
+++ ups/build_multi.sh
@@ -8,20 +8,22 @@ Build bits:
 mkdir /tmp/$USER$$
 cd /tmp/$USER$$
 
-setup nova
-setup git
+setup nova, git
 
 git clone ssh://p-ifdhc@cdcvs.fnal.gov/cvs/projects/ifdhc/ifdhc.git
-git clone ssh://p-ifdh-art@cdcvs.fnal.gov/cvs/projects/ifdh-art/ifdh-art.git
+git clone ssh://p-ifdh-art@cdcvs.fnal.gov/cvs/projects/ifdh-art/ifdh_art.git
 
-. ifdh-art/ups/build_node_setup.sh
+. ifdh_art/ups/build_node_setup.sh
 
-(cd ifdhc &&  buildifdhc.sh $buildargs)
-(cd ifdh-art &&  buildifdh_art.sh $buildargs)
+(cd ifdhc && sh  buildifdhc.sh $buildargs)
+cd ifdhc ; setup -. ifdhc $DECLAREBITS ; cd ..
+(cd ifdh_art && sh  buildifdh_art.sh $buildargs)
 
+export VERSION=v1_1_1
 (cd ifdhc && upd addproduct -r `pwd` -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
-(cd ifdh-art && upd addproduct -r `pwd` -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
+(cd ifdh_art && upd addproduct -r `pwd` -M ups -m ifdh_art.table $DECLAREBITS ifdh_art $VERSION)
 
+cd 
 rm -rf /tmp/$USER$$
 
 EOF
