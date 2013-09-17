96e06bed97aa5527377ab5b63fc56535c55e0686
Marc Mengel <mengel@fnal.gov>
2013-09-17 11:46:44 -0500

    version 1_2_1a

--- ups/build_multi.sh
+++ ups/build_multi.sh
@@ -12,10 +12,10 @@ PRODUCTS=`pwd`/p:/nusoft/app/externals:$PRODUCTS
 setup git, upd
 git clone ssh://p-ifdhc@cdcvs.fnal.gov/cvs/projects/ifdhc/ifdhc.git
 git clone ssh://p-ifdh-art@cdcvs.fnal.gov/cvs/projects/ifdh-art/ifdh_art.git
-export VERSION=v1_2_0
+export VERSION=v1_2_1
 . ifdh_art/ups/build_node_setup.sh
 (cd ifdhc && sh  buildifdhc.sh $buildargs)
-cd ifdhc ; ups declare -r `pwd` -M ups -m ifdhc.table ifdhc $VERSION $DECLAREBITS
+(cd ifdhc && ups declare -r `pwd` -M ups -m ifdhc.table ifdhc $VERSION $DECLAREBITS
 (cd ifdh_art && sh  buildifdh_art.sh $buildargs)
 (cd ifdhc && upd addproduct -r `pwd` -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
 (cd ifdh_art && upd addproduct -r `pwd` -M ups -m ifdh_art.table $DECLAREBITS ifdh_art $VERSION)
