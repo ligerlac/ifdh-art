97ebdce4fa93e39c5d798c0f1c30a410429bdb0c
Marc Mengel <mengel@fnal.gov>
2013-10-06 14:02:32 -0500

    draft of build fixups

--- ups/build_multi.sh
+++ ups/build_multi.sh
@@ -1,5 +1,5 @@
 
-hostlist="novagpvm02 novagpvm03"
+hostlist="novagpvm02 novagpvm03 novadaq-far-datamon"
 
 cat <<'EOF'
 Build bits:
@@ -17,7 +17,10 @@ export VERSION=v1_2_1a
 (cd ifdhc && sh  buildifdhc.sh $buildargs)
 (cd ifdhc && ups declare -z /tmp/$USER$$/p -r `pwd` -M ups -m ifdhc.table ifdhc $VERSION $DECLAREBITS)
 (cd ifdh_art && sh  buildifdh_art.sh $buildargs)
-(cd ifdhc && upd addproduct -r `pwd` -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
+mv ifdh_art/Lin*/lib/*beam* ifdhc/Lin*/lib
+mv ifdh_art/inc/*beam* ifdhc/inc
+(cd ifdhc && make distrib && upd addproduct -T ifdhc.tar  -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
+(cd ifdhc && make distrib && upd addproduct -T ifbeam.tar  -M ups -m ifdbeam.table $DECLAREBITS ifbeam $VERSION)
 (cd ifdh_art && upd addproduct -r `pwd` -M ups -m ifdh_art.table $DECLAREBITS ifdh_art $VERSION)
 
 cd 
--- ups/build_node_setup.sh
+++ ups/build_node_setup.sh
@@ -6,4 +6,10 @@ novagpvm02*) export buildargs="debug e4"
 novagpvm03*) export buildargs="prof e4"
                 export DECLAREBITS="-q e4:prof -4"
                  ;;
+novagdaq-far-datamon*) export buildargs="debug e4"
+                export DECLAREBITS="-q debug:e4 -4"
+                ;;
+novagdaq-far-datamon-p*) export buildargs="prof e4"
+                export DECLAREBITS="-q e4:prof -4"
+                ;;
 esac
