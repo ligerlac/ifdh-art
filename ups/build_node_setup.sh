cade425bb91407d76c1b66412fa2caa593c64906
Marc Mengel <mengel@fnal.gov>
2013-10-07 16:12:02 -0500

    build stuff

--- ups/build_node_setup.sh
+++ ups/build_node_setup.sh
@@ -1,5 +1,5 @@
 
-case `hostname`-$BUILD_EXTRA in
+case `hostname`$BUILD_EXTRA in
 novagpvm02*) export buildargs="debug e4"
                 export DECLAREBITS="-q debug:e4 -4"
                 ;;
@@ -10,7 +10,7 @@ novagdaq-far-datamon*) export buildargs="debug e4"
                 export DECLAREBITS="-q debug:e4 -4"
 		. /online_monitor/externals/setup
                 ;;
-novagdaq-far-datamon-p*) export buildargs="prof e4"
+novagdaq-far-datamon*prof*) export buildargs="prof e4"
                 export DECLAREBITS="-q e4:prof -4"
 		. /online_monitor/externals/setup
                 ;;
