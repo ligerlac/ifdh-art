4102ace77bfa89df8ab2f512ff09dc31448d4318
Marc Mengel <mengel@fnal.gov>
2013-10-07 16:13:33 -0500

    build stuff

--- ups/build_node_setup.sh
+++ ups/build_node_setup.sh
@@ -6,12 +6,12 @@ novagpvm02*) export buildargs="debug e4"
 novagpvm03*) export buildargs="prof e4"
                 export DECLAREBITS="-q e4:prof -4"
                  ;;
-novagdaq-far-datamon*) export buildargs="debug e4"
-                export DECLAREBITS="-q debug:e4 -4"
+novadaq-far-datamon*prof*) export buildargs="prof e4"
+                export DECLAREBITS="-q e4:prof -4"
 		. /online_monitor/externals/setup
                 ;;
-novagdaq-far-datamon*prof*) export buildargs="prof e4"
-                export DECLAREBITS="-q e4:prof -4"
+novadaq-far-datamon*) export buildargs="debug e4"
+                export DECLAREBITS="-q debug:e4 -4"
 		. /online_monitor/externals/setup
                 ;;
 esac
