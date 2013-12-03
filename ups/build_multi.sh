4f63064f64b96af197d98b4310439db7976b73b2
Marc Mengel <menel@fnal.gov>
2013-12-03 10:48:32 -0600

    v1_2_9

--- ups/build_multi.sh
+++ ups/build_multi.sh
@@ -8,11 +8,13 @@ Build bits:
 mkdir /tmp/$USER$$
 cd /tmp/$USER$$
 mkdir p
-PRODUCTS=`pwd`/p:/nusoft/app/externals:$PRODUCTS
+. /online_monitor/externals/setup
+. /nusoft/app/externals/setup
+PRODUCTS=`pwd`/p:$PRODUCTS
 setup git, upd
 git clone ssh://p-ifdhc@cdcvs.fnal.gov/cvs/projects/ifdhc/ifdhc.git
 git clone ssh://p-ifdh-art@cdcvs.fnal.gov/cvs/projects/ifdh-art/ifdh_art.git
-export VERSION=v1_2_7
+export VERSION=v1_2_9
 . ifdh_art/ups/build_node_setup.sh
 (cd ifdhc && sh  buildifdhc.sh $buildargs)
 (cd ifdhc && ups declare -z /tmp/$USER$$/p -r `pwd` -M ups -m ifdhc.table ifdhc $VERSION $DECLAREBITS)
@@ -20,6 +22,7 @@ export VERSION=v1_2_7
 mv ifdh_art/Lin*/lib/*Beam* ifdhc/Lin*/lib
 mv ifdh_art/Lin*/inc/*Beam* ifdhc/inc
 (cd ifdhc && make distrib )
+#          
 (cd ifdhc && upd addproduct -T ifdhc.tar.gz  -M ups -m ifdhc.table $DECLAREBITS ifdhc $VERSION)
 (cd ifdhc && upd addproduct -T ifbeam.tar.gz  -M ups -m ifbeam.table $DECLAREBITS ifbeam $VERSION)
 (cd ifdh_art && upd addproduct -r `pwd` -M ups -m ifdh_art.table $DECLAREBITS ifdh_art $VERSION)
