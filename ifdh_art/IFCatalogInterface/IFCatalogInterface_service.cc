ef983595fb6c2a4fc4c601c99242efa9926c98ff
Kyle Knoepfel <knoepfel@fnal.gov>
2022-03-08 14:06:44 -0600

    Add missing header include.

--- ifdh_art/IFCatalogInterface/IFCatalogInterface_service.cc
+++ ifdh_art/IFCatalogInterface/IFCatalogInterface_service.cc
@@ -1,4 +1,4 @@
 #include "IFCatalogInterface_service.h"
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
 
-//DEFINE_ART_SERVICE(ifdh_ns::IFCatalogInterface);
 DEFINE_ART_SERVICE_INTERFACE_IMPL(ifdh_ns::IFCatalogInterface, art::CatalogInterface)
