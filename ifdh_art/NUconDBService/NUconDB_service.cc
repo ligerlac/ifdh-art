128bdf2d9385467c5805a203e08d417097ac12fa
Chris Backhouse <cjbackhouse@gmail.com>
2022-03-08 18:03:44 +0000

    Split includes of ServiceMacros.h into declaration and definition parts to avoid warnings from art.

--- ifdh_art/NUconDBService/NUconDB_service.cc
+++ ifdh_art/NUconDBService/NUconDB_service.cc
@@ -1,4 +1,6 @@
 #include "NUconDB_service.h"
 
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+
 //DEFINE_ART_SERVICE(nucondb_ns::nucondbService)
 DEFINE_ART_SERVICE(nucondb_ns::NUconDBService)
