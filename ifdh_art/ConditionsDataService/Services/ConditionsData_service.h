3ff7e86a183f208d9bbfccc0fe57496e77da260c
ligerlac <lino.oscar.gerlach@cern.ch>
2023-05-19 04:57:17 +0200

    removed unneeded includes

--- ifdh_art/ConditionsDataService/Services/ConditionsData_service.h
+++ ifdh_art/ConditionsDataService/Services/ConditionsData_service.h
@@ -1,11 +1,5 @@
 #ifndef CONDITIONSDATASERVICE_H
 #define CONDITIONSDATASERVICE_H
-#include <memory>
-
-#include "fhiclcpp/ParameterSet.h"
-#include "fhiclcpp/types/Atom.h"
-#include "fhiclcpp/types/Name.h"
-#include "fhiclcpp/types/Comment.h"
 
 #include "art/Framework/Services/Registry/ActivityRegistry.h"
 #include "art/Framework/Services/Registry/ServiceDeclarationMacros.h"
